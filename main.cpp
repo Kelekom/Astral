51acc34b007eec628373ab185479108832931085
Kelekom <RyanopsGaming@yahoo.com>
2019-08-07 11:42:28 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<SFML/Graphics.hpp>
+#include"include/Game.hpp"
+#include"include/AssetManager.hpp"
+#include"include/InputManager.hpp"
+#include"include/TileMap.hpp"
+#include"include/AnimatedSprite.hpp"
+
+int main()
+{
+
+    Game game;
+
+    game.Run();
+
+    return 0;
+}
